7713b1b2f3a5067f5d01cd5db4deeda7326e404b
shinndev <aisyxaisyx@gmail.com>
2021-03-02 15:49:54 +0300

    Stuff

---
+++ src/pkg/sbase/files/config.mk
@@ -0,0 +1,15 @@
+# sbase version
+VERSION = 0.0
+
+# paths
+MANPREFIX = /share/man
+
+CC = cc
+AR = ar
+RANLIB = ranlib
+
+# for NetBSD add -D_NETBSD_SOURCE
+# -lrt might be needed on some systems
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_FILE_OFFSET_BITS=64
+CFLAGS   = -std=c99 -Wall -pedantic
+LDFLAGS  = -s
---
+++ src/pkg/ubase/files/config.mk
@@ -0,0 +1,14 @@
+# ubase version
+VERSION = 0.1
+
+# paths
+MANPREFIX = /share/man
+
+CC = cc
+AR = ar
+RANLIB = ranlib
+
+CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
+CFLAGS   = -std=c99 -Wall -Wextra
+LDLIBS   = -lcrypt
+LDFLAGS  = -s
